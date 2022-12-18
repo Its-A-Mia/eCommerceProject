f695f6f0e4922adf512d805114ac96e0e4789912
Its-A-Mia <miapurdun@gmail.com>
2022-12-18 04:29:23 -0600

    edited both components

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import { Container } from "@mui/material";
+import Head from "next/head";
+import NavBar from "./NavBar";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>eCommerce Site</title>
+        <meta name="description" content="eCommerce practice site" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <Container xs={12}>
+        <main>{children}</main>
+      </Container>
+      <footer></footer>
+    </>
+  );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,42 +1,45 @@
-import { AppBar, Grid, Box, Divider, TextField } from "@mui/material";
+import { Grid, Box, Divider, TextField } from "@mui/material";
+import { Container } from "@mui/system";
 import Link from "next/link";
 
 export default function () {
   return (
     <>
-      <Box display="flex" justifyContent="center" bgcolor="gray">
-        <Grid container spacing={5} width="72.5%" padding="10px">
-          <Grid item xs={12}>
-            <Box display="flex" justifyContent="flex-end" gap="50px" alignItems="center">
-              <Link href="/work-in-progress">Link</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Link</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Link</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Link</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Link</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Link</Link>
-            </Box>
+      <Box bgcolor="#EEEEEE">
+        <Container>
+          <Grid container spacing={5} p="20px">
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="flex-end" gap="50px" alignItems="center">
+                <Link href="/work-in-progress">Link</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Link</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Link</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Link</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Link</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Link</Link>
+              </Box>
+            </Grid>
+            <Grid item xs={2} display="flex" justifyContent="center" alignItems="center">
+              <Link href="/">Logo</Link>
+            </Grid>
+            <Grid item xs={5}>
+              <TextField id="outlined-basic" label="Enter Item ID" fullWidth />
+            </Grid>
+            <Grid item xs={5} display="flex" justifyContent="center" alignItems="center">
+              <Box display="flex" gap="50px">
+                <Link href="/auth">Sign In / Register</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/work-in-progress">Orders</Link>
+                <Divider orientation="vertical" flexItem />
+                <Link href="/cart">Cart</Link>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
-            <div>Logo</div>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField id="outlined-basic" label="Enter Item ID" fullWidth />
-          </Grid>
-          <Grid item xs={4}>
-            <Box display="flex" justifyContent="flex-end" gap="50px" alignItems="center">
-              <Link href="/auth">Sign In / Register</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/work-in-progress">Orders</Link>
-              <Divider orientation="vertical" flexItem />
-              <Link href="/cart">Cart</Link>
-            </Box>
-          </Grid>
-        </Grid>
+        </Container>
       </Box>
     </>
   );
