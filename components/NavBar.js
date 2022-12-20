7748f09d3396c6077089b1944a5425a736ae1318
Its-A-Mia <miapurdun@gmail.com>
2022-12-19 18:07:24 -0600

    Added logo to NavBar, cleaned up CSS.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,9 @@
-import { Grid, Box, Divider, TextField, useMediaQuery } from "@mui/material";
+import { Grid, Box, Divider, TextField } from "@mui/material";
 import { Container } from "@mui/system";
+import eShopLogo from "../images/eShop.png";
 import Link from "next/link";
+import Image from "next/image";
+import NavBarStyles from "../styles/NavBar.module.css";
 
 export default function () {
   const hiddenOnMobile = {
@@ -14,24 +17,43 @@ export default function () {
           <Grid container spacing={3} p="20px">
             <Grid item xs={12} sx={hiddenOnMobile} justifyContent="flex-end">
               <Box display="flex" gap="30px">
-                <Link href="/work-in-progress">While Supplies Last</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  While Supplies Last
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">What's New</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  What's New
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">Same-Day</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  Same-Day
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">Online-Only</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  Online-Only
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">Email Offers</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  Email Offers
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">Customer Service</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  Customer Service
+                </Link>
               </Box>
             </Grid>
-            <Grid item xs={2} display="flex" justifyContent="center" alignItems="center">
-              <Link href="/">Logo</Link>
+            <Grid item xs={4} md={2} display="flex" justifyContent="center" alignItems="center">
+              <Link href="/">
+                <Image src={eShopLogo} width={150} alt="eShop Logo" />
+              </Link>
             </Grid>
-            <Grid item xs={5}>
-              <TextField id="outlined-basic" label="Enter Item ID" fullWidth />
+            <Grid item md={5} sx={hiddenOnMobile}>
+              <TextField
+                id="outlined-basic"
+                label="Enter Item ID"
+                fullWidth
+                sx={{ background: "white" }}
+              />
             </Grid>
             <Grid
               item
@@ -42,11 +64,17 @@ export default function () {
               sx={hiddenOnMobile}
             >
               <Box display="flex" gap="50px">
-                <Link href="/auth">Sign In / Register</Link>
+                <Link href="/auth" className={NavBarStyles.linkStyle}>
+                  Sign In / Register
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/work-in-progress">Orders</Link>
+                <Link href="/work-in-progress" className={NavBarStyles.linkStyle}>
+                  Orders
+                </Link>
                 <Divider orientation="vertical" flexItem />
-                <Link href="/cart">Cart</Link>
+                <Link href="/cart" className={NavBarStyles.linkStyle}>
+                  Cart
+                </Link>
               </Box>
             </Grid>
           </Grid>
