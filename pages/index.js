060f04cc378b0e6fe954f749ae2f79b7ea8a9d56
Its-A-Mia <miapurdun@gmail.com>
2022-12-20 15:23:55 -0600

    destructured the category cards to make it scalable.

--- pages/index.js
+++ pages/index.js
@@ -5,87 +5,77 @@ import { Typography, Grid, Box } from "@mui/material";
 import Button from "@mui/material/Button";
 
 export default function () {
-  const buttonBackground = (bgStyle) => {
-    const bg = `${homeStyles.categoriesButton} ${bgStyle}`;
-    console.log(bg);
-    return bg;
+  //   const buttonBackground = (bgStyle) => {
+  //     const bg = `${homeStyles.categoriesButton} ${bgStyle}`;
+  //     console.log(bg);
+  //     return bg;
+  //   };
+
+  // iterate through to create category buttons using an array of categories
+  const createCategoryButtons = (categoryArr) => {
+    let categoryCards = [];
+    for (let i = 0; i < categoryArr.length; i++) {
+      categoryCards.push(
+        <Grid item xs={12} sm={6} md={4} className={homeStyles.categoryGrid}>
+          <Button
+            href={categoryArr[i].href}
+            className={`${categoryArr[i].style}`}
+            variant="contained"
+            fullWidth
+            size="large"
+          >
+            {categoryArr[i].category}
+          </Button>
+        </Grid>
+      );
+    }
+    return categoryCards;
   };
+
+  // array of categories to create
+  let categoryArr = [
+    {
+      category: "T-Shirts",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton}`,
+    },
+    {
+      category: "Sweaters",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton} ${homeStyles.tShirtBackground}`,
+    },
+    {
+      category: "Hoodies",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton} ${homeStyles.tShirtBackground}`,
+    },
+    {
+      category: "Sweatpants",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton} ${homeStyles.tShirtBackground}`,
+    },
+    {
+      category: "Jeans",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton} ${homeStyles.tShirtBackground}`,
+    },
+    {
+      category: "Shoes",
+      href: "work-in-progress",
+      style: `${homeStyles.categoriesButton} ${homeStyles.tShirtBackground}`,
+    },
+  ];
+
+  console.log(headerStyles.categoriesButton);
+
+  //construct categories
+  let categoryButtonArr = createCategoryButtons(categoryArr);
+
   return (
     <>
-      {/* <Head>
-        <title></title>
-      </Head> */}
-      <main>
-        <Grid container spacing={2} marginTop="60px">
-          <Grid item xs={12}></Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={buttonBackground(homeStyles.tShirtBackground)}
-            >
-              T-Shirts
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={homeStyles.categoriesButton}
-            >
-              Sweaters
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={homeStyles.categoriesButton}
-            >
-              Hoodies
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={homeStyles.categoriesButton}
-            >
-              Sweatpants
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={homeStyles.categoriesButton}
-            >
-              Jeans
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              href="/work-in-progress"
-              variant="contained"
-              fullWidth
-              size="large"
-              className={homeStyles.categoriesButton}
-            >
-              Shoes
-            </Button>
-          </Grid>
-        </Grid>
-      </main>
+      <Grid container spacing={2} marginTop="60px">
+        {categoryButtonArr}
+      </Grid>
     </>
   );
 }
