e9256618ee34d23e0d74b8f225e8e9acc209ef7e
Its-A-Mia <miapurdun@gmail.com>
2022-12-29 11:30:03 -0600

    added some functionality to user api route

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,7 +1,20 @@
 import prisma from "../../../lib/prisma";
+import hashPass from "../../../lib/hashPass";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    const hashedPassword = await hashPass(password);
+
+    const postResult = await prisma.user.create({
+      data: {
+        email,
+        hash: hashedPassword,
+      },
+    });
+
+    res.json(postResult);
   } else if (req.method === "GET") {
   } else {
     throw new Error(`The HTTP ${req.method} method is not suported at this route`);
