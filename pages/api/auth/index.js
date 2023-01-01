dfbacac0b445a5f79b1701af6c041e7b3926c01c
Its-A-Mia <miapurdun@gmail.com>
2022-12-31 22:46:54 -0600

    added api endpoints for auth and checkout

---
+++ pages/api/auth/index.js
@@ -0,0 +1,34 @@
+import prisma from "../../../lib/prisma";
+import comparePassAndHash from "../../../lib/checkHash";
+import { SignJWT } from "jose";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    const existingUser = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (!existingUser) {
+      res.status(400).send("There is no account associated with this email.");
+      return;
+    }
+
+    const compareResult = await comparePassAndHash(password, existingUser.hash);
+
+    if (!compareResult) {
+      res.status(400).send("Incorrect Password.");
+      return;
+    }
+
+    const token = await new SignJWT({ id: existingUser.id })
+      .setProtectedHeader({ alg: "HS256" })
+      .setExpirationTime("15m")
+      .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+
+    res.json({ token });
+  } else {
+    res.status(501).send("The HTTP method is not supported by this route and cannot be handled.");
+  }
+}
