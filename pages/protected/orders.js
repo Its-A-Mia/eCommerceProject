d8222db1acec290cd14c85af259663d2c4355f47
Its-A-Mia <miapurdun@gmail.com>
2023-01-02 15:03:19 -0600

    created protected route to house authenticated routes

---
+++ pages/protected/orders.js
@@ -0,0 +1,13 @@
+import PrimaryLayout from "../../components/PrimaryLayout";
+
+export default function Orders(props) {
+  // have auth functionality connected to seeing your order and checking out. No checkout page, just a checkout button that checks session auth, if no auth, redirect to auth.
+
+  // cancel order is a delete request, have an ARE YOU SURE button. cancel multiple orders functionality?
+
+  return (
+    <>
+      <h1>Order Page</h1>
+    </>
+  );
+}
