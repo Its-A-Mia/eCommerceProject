4da5d1a5f243adfb5f1689969e86b6d8ec7580a2
Its-A-Mia <miapurdun@gmail.com>
2023-01-02 15:41:34 -0600

    created foundation for each product page

--- pages/products/tops/hoodies.js
+++ pages/products/tops/hoodies.js
@@ -1,16 +1,38 @@
-import PrimaryLayout from "../../../components/PrimaryLayout";
 import ProductsLayout from "../../../components/ProductsLayout";
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import productStyles from "../../../styles/products.module.css";
 
-export default function Hoodies() {
-  return <></>;
-}
+export const getStaticProps = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
 
-Hoodies.getLayout = function getLayout(page) {
+  return {
+    props: { products: data },
+  };
+};
+
+export default function Hoodies({ products }) {
   const productInfo = { name: "Hoodies", path: "Tops" };
 
   return (
-    <PrimaryLayout>
-      <ProductsLayout productsPage={productInfo}>{page}</ProductsLayout>
-    </PrimaryLayout>
+    <>
+      <ProductsLayout productsPage={productInfo}>
+        <Grid container spacing={2}>
+          {products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} className={productStyles.productCard} key={product.id}>
+              <Card sx={{ width: "100%", height: "100%" }}>
+                <CardActionArea>
+                  <CardMedia component="img" height="270" image={product.image} />
+                  <CardContent>
+                    <Typography>{product.title}</Typography>
+                    <Typography fontWeight="bold">${product.price}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </ProductsLayout>
+    </>
   );
-};
+}
--- pages/products/tops/t-shirts.js
+++ pages/products/tops/t-shirts.js
@@ -1,4 +1,3 @@
-import PrimaryLayout from "../../../components/PrimaryLayout";
 import ProductsLayout from "../../../components/ProductsLayout";
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import productStyles from "../../../styles/products.module.css";
@@ -13,33 +12,27 @@ export const getStaticProps = async () => {
 };
 
 export default function TShirts({ products }) {
-  return (
-    <>
-      <Grid container spacing={2}>
-        {products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} className={productStyles.productCard} key={product.id}>
-            <Card sx={{ width: "100%", height: "100%" }}>
-              <CardActionArea>
-                <CardMedia component="img" height="270" image={product.image} />
-                <CardContent>
-                  <Typography>{product.title}</Typography>
-                  <Typography fontWeight="bold">${product.price}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </>
-  );
-}
-
-TShirts.getLayout = function getLayout(page) {
   const productInfo = { name: "T-Shirts", path: "Tops" };
 
   return (
-    <PrimaryLayout>
-      <ProductsLayout productsPage={productInfo}>{page}</ProductsLayout>
-    </PrimaryLayout>
+    <>
+      <ProductsLayout productsPage={productInfo}>
+        <Grid container spacing={2}>
+          {products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} className={productStyles.productCard} key={product.id}>
+              <Card sx={{ width: "100%", height: "100%" }}>
+                <CardActionArea>
+                  <CardMedia component="img" height="270" image={product.image} />
+                  <CardContent>
+                    <Typography>{product.title}</Typography>
+                    <Typography fontWeight="bold">${product.price}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </ProductsLayout>
+    </>
   );
-};
+}
