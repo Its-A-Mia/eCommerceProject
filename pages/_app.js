1ff3347519b6846c92e6c5b2983846b172dd9b68
Its-A-Mia <miapurdun@gmail.com>
2023-01-02 15:01:26 -0600

    adjusted logic for deciding what button the layout should show; also added comments.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import PrimaryLayout from "../components/PrimaryLayout";
 import "../styles/globals.css";
 import store from "../store";
+import App from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function customApp({ Component, pageProps }) {
+  // console.log(pageProps);
+
   const renderWithLayout =
     Component.getLayout ||
     ((page) => (
@@ -14,3 +17,28 @@ export default function App({ Component, pageProps }) {
 
   return renderWithLayout(<Component {...pageProps} />);
 }
+
+customApp.getInitialProps = async (appContext) => {
+  // getInitialProps may be deprecated, not sure of another way to grab cookie and change layout based on result
+  const pageProps = await App.getInitialProps(appContext);
+  // set up prop to decide what the layout should present
+  const authAndProfile = {
+    auth: "auth",
+    profile: "profile",
+  };
+
+  // checks token on initial render--NavBar catches prop, then sets state depending on object value
+  if (!appContext.ctx.req?.cookies.userToken) {
+    pageProps.pageProps = {
+      authOrProfile: authAndProfile.auth,
+    };
+  } else {
+    pageProps.pageProps = {
+      authOrProfile: authAndProfile.profile,
+    };
+  }
+
+  return {
+    ...pageProps,
+  };
+};
