03c9dc78f3b525379907e2348fc40e58f3faad58
Its-A-Mia <miapurdun@gmail.com>
2023-01-02 15:00:01 -0600

    create conditional middleware functionality to handle authentication and session persistence

--- middleware.js
+++ middleware.js
@@ -1,20 +1,32 @@
 import { NextResponse } from "next/server";
 import verifyAuth from "./lib/auth";
 
-export async function middleware(req) {
-  const verifiedToken = await verifyAuth(req).catch((err) => {
-    console.error(err.message);
-  });
+//
 
-  if (!verifiedToken) {
-    const redirectURL = req.nextUrl.clone();
-    redirectURL.pathname = "/auth";
-    return NextResponse.redirect(redirectURL);
+export async function middleware(req) {
+  if (req.nextUrl.pathname.startsWith("/protected")) {
+    const verifiedToken = await verifyAuth(req).catch((err) => {
+      console.error(err.message);
+    });
+
+    if (!verifiedToken) {
+      const redirectURL = req.nextUrl.clone();
+      redirectURL.pathname = "/auth";
+      return NextResponse.redirect(redirectURL);
+    }
+
+    return NextResponse.next();
   }
 
-  return NextResponse.next();
-}
+  if (req.nextUrl.pathname.startsWith("/auth")) {
+    const userToken = req.cookies.get("userToken");
 
-export const config = {
-  matcher: ["/orders", "/api/checkout", "/profile"],
-};
+    if (!userToken) {
+      return NextResponse.next();
+    }
+
+    const redirectURL = req.nextUrl.clone();
+    redirectURL.pathname = "/profile";
+    return NextResponse.redirect(redirectURL);
+  }
+}
