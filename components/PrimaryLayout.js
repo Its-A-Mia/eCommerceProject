60da05c0153cb56d8f3bc1042cece60f3d017c5c
Its-A-Mia <miapurdun@gmail.com>
2023-02-09 12:22:18 -0600

    added alt text to image

--- components/PrimaryLayout.js
+++ components/PrimaryLayout.js
@@ -1,14 +1,14 @@
-import { Container, Box, Typography, Icon } from "@mui/material";
-import Head from "next/head";
-import NavBar from "./NavBar";
-import AppBarNav from "./AppBarNav";
-import favicon from "../public/favicon.ico";
-import Link from "next/link";
-import Image from "next/image";
-import nextjslogo from "../public/images/nextjslogo.png";
-import CopyrightIcon from "@mui/icons-material/Copyright";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Container, Box, Typography, Icon } from '@mui/material';
+import Head from 'next/head';
+import NavBar from './NavBar';
+import AppBarNav from './AppBarNav';
+import favicon from '../public/favicon.ico';
+import Link from 'next/link';
+import Image from 'next/image';
+import nextjslogo from '../public/images/nextjslogo.png';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Layout({ children }) {
   return (
@@ -25,38 +25,32 @@ export default function Layout({ children }) {
           <AppBarNav />
         </Box>
       </header>
-      <Container maxWidth="lg" sx={{ p: "20px 0 120px" }}>
+      <Container maxWidth="lg" sx={{ p: '20px 0 120px' }}>
         <Box display="flex" justifyContent="center">
-          <main style={{ width: "100%", display: "inherit", justifyContent: "inherit" }}>
-            {children}
-          </main>
+          <main style={{ width: '100%', display: 'inherit', justifyContent: 'inherit' }}>{children}</main>
         </Box>
       </Container>
       <footer>
-        <Box sx={{ background: "#5D82B3" }}>
+        <Box sx={{ background: '#5D82B3' }}>
           <Container
             maxWidth="lg"
             sx={{
-              p: "20px 0 20px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
+              p: '20px 0 20px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
             }}
           >
             <Box display="flex" gap="10px" maxWidth="210px" alignItems="center">
               <Typography color="white" fontSize="14px">
                 POWERED BY
               </Typography>
-              <Link
-                href="https://nextjs.org/"
-                target="_blank"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                <Image src={nextjslogo.src} width={104} height={50} />
+              <Link href="https://nextjs.org/" target="_blank" style={{ textDecoration: 'none', color: 'white' }}>
+                <Image src={nextjslogo.src} width={104} height={50} alt="Next js logo" />
               </Link>
             </Box>
             <Box display="flex" alignItems="center" gap="5px">
-              <CopyrightIcon fontSize="small" sx={{ color: "white" }} />
+              <CopyrightIcon fontSize="small" sx={{ color: 'white' }} />
               <Typography color="white">Mia Purdun 2022</Typography>
             </Box>
 
@@ -65,16 +59,16 @@ export default function Layout({ children }) {
               <Link
                 href="https://www.linkedin.com/in/mia-purdun/"
                 target="_blank"
-                style={{ textDecoration: "none", color: "white" }}
+                style={{ textDecoration: 'none', color: 'white' }}
               >
-                <LinkedInIcon sx={{ color: "white", fontSize: "xxx-large" }} />
+                <LinkedInIcon sx={{ color: 'white', fontSize: 'xxx-large' }} />
               </Link>
               <Link
                 href="https://github.com/Its-A-Mia/eCommerceProject"
-                style={{ textDecoration: "none", color: "white" }}
+                style={{ textDecoration: 'none', color: 'white' }}
                 target="_blank"
               >
-                <GitHubIcon sx={{ color: "white", fontSize: "xxx-large" }} />
+                <GitHubIcon sx={{ color: 'white', fontSize: 'xxx-large' }} />
               </Link>
             </Box>
           </Container>
