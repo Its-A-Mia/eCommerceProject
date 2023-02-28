0046c5853e4ce7e4fe5437ca30c40a11fe0607a4
Its-A-Mia <miapurdun@gmail.com>
2023-02-27 22:58:01 -0600

    adjusted category card creation to the map method

--- pages/index.js
+++ pages/index.js
@@ -1,93 +1,76 @@
-import tShirtBG from "../public/images/tshirts.jpg";
-import hoodiesBG from "../public/images/hoodies.jpg";
-import jeansBG from "../public/images/jeans.jpg";
-import shoesBG from "../public/images/shoes.jpg";
-import sweatersBG from "../public/images/sweaters.jpg";
-import sweatpantsBG from "../public/images/sweatpants.jpg";
+import tShirtBG from '../public/images/tshirts.jpg';
+import hoodiesBG from '../public/images/hoodies.jpg';
+import jeansBG from '../public/images/jeans.jpg';
+import shoesBG from '../public/images/shoes.jpg';
+import sweatersBG from '../public/images/sweaters.jpg';
+import sweatpantsBG from '../public/images/sweatpants.jpg';
 
-import homeStyles from "../styles/Home.module.css";
-import { Grid } from "@mui/material";
-import Button from "@mui/material/Button";
+import homeStyles from '../styles/Home.module.css';
+import { Grid } from '@mui/material';
+import Button from '@mui/material/Button';
 
 // background-position: center;
 //   background-size: cover;
 //   background-repeat: no-repeat;
 
 export default function home() {
-  // iterate through to create category buttons using an array of categories
-  const createCategoryButtons = (categoryArr) => {
-    let categoryCards = [];
-    for (let i = 0; i < categoryArr.length; i++) {
-      categoryCards.push(
-        <Grid key={i} item xs={12} sm={6} md={4} className={homeStyles.categoryGrid}>
-          <Button
-            href={categoryArr[i].href}
-            className={`${categoryArr[i].style}`}
-            variant="contained"
-            fullWidth
-            size="large"
-          >
-            <div
-              className={homeStyles.categoriesButtonBackground}
-              style={{
-                backgroundImage: `url(${categoryArr[i].backgroundImagePath.src})`,
-              }}
-            ></div>
-            {categoryArr[i].category}
-          </Button>
-        </Grid>
-      );
-    }
-    return categoryCards;
-  };
-
   // array of categories to create
   let categoryArr = [
     {
-      category: "T-Shirts",
-      href: "/products/tops/shirts",
+      category: 'T-Shirts',
+      href: '/products/tops/shirts',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: tShirtBG,
     },
     {
-      category: "Sweaters",
-      href: "/products/tops/sweaters",
+      category: 'Sweaters',
+      href: '/products/tops/sweaters',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: sweatersBG,
     },
     {
-      category: "Hoodies",
-      href: "/products/tops/hoodies",
+      category: 'Hoodies',
+      href: '/products/tops/hoodies',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: hoodiesBG,
     },
     {
-      category: "Sweatpants",
-      href: "/products/bottoms/sweatpants",
+      category: 'Sweatpants',
+      href: '/products/bottoms/sweatpants',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: sweatpantsBG,
     },
     {
-      category: "Jeans",
-      href: "/products/bottoms/jeans",
+      category: 'Jeans',
+      href: '/products/bottoms/jeans',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: jeansBG,
     },
     {
-      category: "Shoes",
-      href: "/products/bottoms/shoes",
+      category: 'Shoes',
+      href: '/products/bottoms/shoes',
       style: `${homeStyles.categoriesButton}`,
       backgroundImagePath: shoesBG,
     },
   ];
 
-  //construct categories
-  let categoryButtonArr = createCategoryButtons(categoryArr);
-
   return (
     <>
       <Grid container spacing={2} marginTop="40px">
-        {categoryButtonArr}
+        {/* construct category buttons */}
+        {categoryArr.map((category, i) => (
+          <Grid key={i} item xs={12} sm={6} md={4} className={homeStyles.categoryGrid}>
+            <Button href={category.href} className={`${category.style}`} variant="contained" fullWidth size="large">
+              <div
+                className={homeStyles.categoriesButtonBackground}
+                style={{
+                  backgroundImage: `url(${category.backgroundImagePath.src})`,
+                }}
+              ></div>
+              {category.category}
+            </Button>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
